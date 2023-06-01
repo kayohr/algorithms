01279987a0a43141a1ecc95e96d68ab3bd24edf4
kayo <kayohramos@gmail.com>
2023-06-01 18:56:34 -0300

    feat: req-1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    
+    number_students = 0
+    for entry, leave in permanence_period:
+        if not (isinstance(entry, int) and isinstance(leave, int)):
+            return None
+        if entry <= target_time <= leave:
+            number_students += 1
+
+    return number_students
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+target_time = 7
+print(study_schedule(permanence_period, target_time))
