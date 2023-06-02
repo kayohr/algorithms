9a8d2b02ca201d650abd57cbf8aab5235e2bbf1e
kayo <kayohramos@gmail.com>
2023-06-02 15:50:45 -0300

    doc: req-3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,21 +1,51 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    # raise NotImplementedError
-
-    if len(first_string) != len(second_string):
-        return False
-
-    sorted_first = sorted(first_string)
-    sorted_second = sorted(second_string)
-
-    return sorted_first == sorted_second
+# def merge_sort(numbers, start=0, end=None):
+#     if end is None:
+#         end = len(numbers)
+#     if (end - start) > 1:
+#         mid = (start + end) // 2
+#         merge_sort(numbers, start, mid)
+#         merge_sort(numbers, mid, end)
+#         merge(numbers, start, mid, end)
 
 
-if __name__ == "__main__":
-    first_string = "aram"
-    second_string = "maar"
-    result = is_anagram(first_string, second_string)
-    print(result)
+# def merge(numbers, start, mid, end):
+#     left = numbers[start:mid]
+#     right = numbers[mid:end]
+
+#     left_index, right_index = 0, 0
+#     for general_index in range(start, end):
+#         if left_index >= len(left):
+#             numbers[general_index] = right[right_index]
+#             right_index = right_index + 1
+#         elif right_index >= len(right):
+#             numbers[general_index] = left[left_index]
+#             left_index = left_index + 1
+#         elif left[left_index] < right[right_index]:
+#             numbers[general_index] = left[left_index]
+#             left_index = left_index + 1
+#         else:
+#             numbers[general_index] = right[right_index]
+#             right_index = right_index + 1
+
+
+# def is_anagram(first_string, second_string):
+#     """Faça o código aqui."""
+#     # raise NotImplementedError
+
+#     if len(first_string) != len(second_string):
+#         return False
+
+#     sorted_first = merge_sort(first_string)
+#     sorted_second = merge_sort(second_string)
+
+#     return sorted_first == sorted_second
+
+
+# if __name__ == "__main__":
+#     first_string = "aram"
+#     second_string = "maar"
+#     result = is_anagram(first_string, second_string)
+#     print(result)
 
 # Site apoio:
 # https://www.geeksforgeeks.org/check-whether-two-strings-are-anagram-of-each-other/
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word[low_index] == word[high_index]:
-        return True
-    if word[low_index] != word[high_index]:
+    if word == "":
         return False
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index].lower() != word[high_index].lower():
+        return False
+
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
 
